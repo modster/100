ce2ca9865ccfb34b3bb382a375eb1255d4f983ed
E. M. Greeff <unmodster@gmail.com>
2022-09-13 19:15:42 -0600

    debugger fixed

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-window.addEventListener('load', function () {
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight;
+// window.addEventListener('load', function () {
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight;
 
-    function drawShape(radius) {
-        ctx.beginPath();
-        ctx.save();
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.moveTo(0, 0);
-        ctx.lineTo(0, 0 - radiuus)
-        ctx.restore();
-        ctx.closePath();
-        ctx.straoke();
+function drawShape(radius) {
+    ctx.beginPath();
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, 0 - radius)
+    ctx.restore();
+    ctx.closePath();
+    ctx.stroke();
 
-    }
-    drawShape(100);
-
-});
+}
+drawShape(100);
+// });
